aeaaf532d5688f93fa3b337ec4c2f99245f52d7a
aristofun <mbutlitsky@gmail.com>
2019-12-02 12:40:05 +0200

    English localization - weather API

--- weather.js
+++ weather.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 // Weather API params
 const wtUrl = `https://api.darksky.net/forecast/${process.env.WEATHER_TOKEN}/`;
-const wtParams = {params: {units: 'si', lang: 'ru', exclude: ['alerts', 'flags', 'daily']}};
+const wtParams = {params: {units: 'si', lang: 'en', exclude: ['alerts', 'flags', 'daily']}};
 
 function weather(latitude, longitude) {
   return axios.get(`${wtUrl}${latitude},${longitude}`, wtParams);
